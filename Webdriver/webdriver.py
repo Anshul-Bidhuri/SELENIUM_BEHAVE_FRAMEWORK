c9a6abee634d8eb16bdaf03b0cf65a1342cdd60f
Anshul-Bidhuri <anshulbidhuri9876@gmail.com>
2023-01-08 20:46:35 +0530

    cross browser added

--- Webdriver/webdriver.py
+++ Webdriver/webdriver.py
@@ -2,6 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
 
-def get_chrome_webdriver():
-    driver = webdriver.Chrome(service=Service("chromedriver_108.exe"))
+def get_webdriver(webdriver_type):
+    if webdriver_type == "edge":
+        driver = webdriver.Edge()
+    elif webdriver_type == "firefox":
+        driver = webdriver.Firefox()
+    else:
+        driver = webdriver.Chrome(service=Service("chromedriver_108.exe"))
     return driver
--- features/environment.py
+++ features/environment.py
@@ -1,8 +1,14 @@
 from Webdriver import webdriver
 import logging
 
+
+def before_all(context):
+    browser_type = context.config.userdata.get("browser")
+    context.browser_type = browser_type
+
+
 def before_scenario(context, scenario):
-    context.driver = logging.FileHandler.selenium_driver = webdriver.get_chrome_webdriver()
+    context.driver = logging.FileHandler.selenium_driver = webdriver.get_webdriver(context.browser_type)
     context.driver.maximize_window()
 
 
